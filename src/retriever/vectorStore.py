20f9cacbd3fc478037d208e1bad6e6b6edb8e613
Pavan Gudiwada <pavan.gudiwada@fau.de>
2025-11-15 22:07:14 +0100

    correcting the module import

--- src/retriever/vectorStore.py
+++ src/retriever/vectorStore.py
@@ -3,7 +3,7 @@ import torch
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from huggingface_hub import login
 
